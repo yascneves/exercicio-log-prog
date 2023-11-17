30e909a6b42bad2b0b294559b7dd21e7550e75af
Yasmim Neves <97295675+yascneves@users.noreply.github.com>
2023-11-16 22:33:27 -0300

    Update exercicio-loop.cpp

--- exercicio-loop.cpp
+++ exercicio-loop.cpp
@@ -1,1 +1,24 @@
+include <stdio.h>
 
+//Exercicio utilizando estrutura de repetição com teste no início
+
+int main (){
+	int qtd;
+	float preco, acumulador=0;
+	
+	printf("Digite a qtd\n");
+	scanf("%d", &qtd);
+	
+	
+	while(qtd>0){
+		printf("Digite o preco\n");
+		scanf("%f", &preco);
+	
+		acumulador = acumulador+qtd*preco;
+		printf("O valor parcial da compra eh: %f", acumulador);
+		printf("Digite a qtd\n");
+		scanf("%d", &qtd);
+	
+	}
+	printf("A soma total dos produtos eh: %f", acumulador);
+}
